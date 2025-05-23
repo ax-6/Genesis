24cfd38c61958b52c02be67a8a0e7f42eb7fba49
Xie Zicong <xie.zicong@axa6.com>
2025-05-23 22:23:42 +0800

    Added WebView2 functions in vm.

--- Windows/Windows/main.cpp
+++ Windows/Windows/main.cpp
@@ -1,9 +1,4 @@
-// Copyright 2025 AQ author, All Rights Reserved.
-// This program is licensed under the AQ License. You can find the AQ license in
-// the root directory.
-
-#include <iostream>
-#include <map>
+// compile with: /D_UNICODE /DUNICODE /DWIN32 /D_WINDOWS /c
 
 #include <windows.h>
 #include <stdlib.h>
@@ -11,93 +6,53 @@
 #include <tchar.h>
 #include <wrl.h>
 #include <wil/com.h>
-
+// <IncludeHeader>
+// include WebView2 header
 #include "WebView2.h"
+// </IncludeHeader>
 
 #include "main.h"
 
-static TCHAR szWindowClass[] = _T("Project Genesis");
 
-static TCHAR szTitle[] = _T("Project Genesis");
+using namespace Microsoft::WRL;
 
-HINSTANCE hInst;
-
-LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
-
-static wil::com_ptr<ICoreWebView2Controller> page_webviewController;
-
-static wil::com_ptr<ICoreWebView2> page_webview;
-
-std::map<HWND, wil::com_ptr<ICoreWebView2Controller>> g_webViewControllers;
-std::map<HWND, wil::com_ptr<ICoreWebView2>> g_webViews;
-static wil::com_ptr<ICoreWebView2Environment> g_webViewEnvironment;
-
-
-HINSTANCE global_hInstance;
-HINSTANCE global_hPrevInstance;
-LPSTR     global_lpCmdLine;
-int       global_nCmdShow;
-
-const char* global_window_class; const char* global_title; int global_length; int global_height; const char* global_url;
-
-LPCWSTR global_url_buffer;
-
-void CreateWebView(HWND hwnd) {
-	if (!g_webViewEnvironment) return;
-
-	g_webViewEnvironment->CreateCoreWebView2Controller(
-		hwnd,
-		Microsoft::WRL::Callback<ICoreWebView2CreateCoreWebView2ControllerCompletedHandler>(
-			[hwnd](HRESULT result, ICoreWebView2Controller* controller) -> HRESULT {
-				if (SUCCEEDED(result)) {
-
-					controller->get_CoreWebView2(&g_webViews[hwnd]);
-
-					wil::com_ptr<ICoreWebView2Settings> settings;
-					g_webViews[hwnd]->get_Settings(&settings);
-					settings->put_IsScriptEnabled(TRUE);
-					settings->put_AreDefaultScriptDialogsEnabled(TRUE);
-					settings->put_IsWebMessageEnabled(TRUE);
-
-					RECT bounds;
-					GetClientRect(hwnd, &bounds);
-					controller->put_Bounds(bounds);
-
-					g_webViewControllers[hwnd] = controller;
-
-					//MessageBox(NULL, global_url_buffer, L"Debug", MB_OK);
-
-					g_webViews[hwnd]->Navigate(global_url_buffer);
-				}
-				return S_OK;
-			}).Get());
-
+// UTF-8 to UTF-16
+std::wstring widen(const std::string& str) {
+    int size_needed = MultiByteToWideChar(CP_UTF8, 0, str.c_str(), (int)str.size(), NULL, 0);
+    std::wstring result(size_needed, 0);
+    MultiByteToWideChar(CP_UTF8, 0, str.c_str(), (int)str.size(), &result[0], size_needed);
+    return result;
 }
 
+struct WindowParams {
+    std::wstring cls_name;
+    std::wstring title;
+    int width;
+    int height;
+    std::wstring url;
+};
 
+// 窗口过程函数
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+    switch (msg) {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+    default:
+        return DefWindowProc(hwnd, msg, wParam, lParam);
+    }
+}
 
-DWORD WINAPI WindowsStartAsync(LPVOID lpParam)
-{
-	const char* window_class = global_window_class; const char* title= global_title; int length= global_length; int height= global_height; const char* url= global_url;
+DWORD WINAPI CreateWebView2Window(LPVOID lpParam) {
+    auto params = reinterpret_cast<WindowParams*>(lpParam);
 
-	HINSTANCE hInstance = GetModuleHandle(NULL);
-	HINSTANCE hPrevInstance = global_hPrevInstance;
-	LPSTR     lpCmdLine = global_lpCmdLine;
-	int       nCmdShow = global_nCmdShow;
+	HRESULT hr = CoInitializeEx(NULL, COINIT_APARTMENTTHREADED);
+	if (FAILED(hr)) {
+		delete reinterpret_cast<WindowParams*>(lpParam);
+		return 1;
+	}
 
-	int wc_length = MultiByteToWideChar(CP_UTF8, 0, window_class, -1, NULL, 0);
-	wchar_t* wc_str = new wchar_t[wc_length];
-	MultiByteToWideChar(CP_UTF8, 0, window_class, -1, wc_str, wc_length);
-
-	int title_length = MultiByteToWideChar(CP_UTF8, 0, title, -1, NULL, 0);
-	wchar_t* title_str = new wchar_t[title_length];
-	MultiByteToWideChar(CP_UTF8, 0, title, -1, title_str, title_length);
-
-
-	int url_length = MultiByteToWideChar(CP_UTF8, 0, url, -1, NULL, 0);
-	wchar_t* wideString = new wchar_t[url_length];
-	MultiByteToWideChar(CP_UTF8, 0, url, -1, wideString, url_length);
-	global_url_buffer = wideString;
+	//CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
 
 	WNDCLASSEX wcex;
 
@@ -106,12 +61,12 @@ DWORD WINAPI WindowsStartAsync(LPVOID lpParam)
 	wcex.lpfnWndProc = WndProc;
 	wcex.cbClsExtra = 0;
 	wcex.cbWndExtra = 0;
-	wcex.hInstance = hInstance;
-	wcex.hIcon = LoadIcon(hInstance, IDI_APPLICATION);
+	wcex.hInstance = GetModuleHandle(NULL);
+	wcex.hIcon = LoadIcon(GetModuleHandle(NULL), IDI_APPLICATION);
 	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
 	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
 	wcex.lpszMenuName = NULL;
-	wcex.lpszClassName = wc_str;
+	wcex.lpszClassName = params->cls_name.c_str();
 	wcex.hIconSm = LoadIcon(wcex.hInstance, IDI_APPLICATION);
 
 	if (!RegisterClassEx(&wcex))
@@ -121,40 +76,31 @@ DWORD WINAPI WindowsStartAsync(LPVOID lpParam)
 			_T("Windows Desktop Guided Tour"),
 			NULL);
 
-		DWORD error = GetLastError(); 
-
-		LPVOID lpMsgBuf;
-		FormatMessage(
-			FORMAT_MESSAGE_ALLOCATE_BUFFER |
-			FORMAT_MESSAGE_FROM_SYSTEM |
-			FORMAT_MESSAGE_IGNORE_INSERTS,
-			NULL,
-			error,
-			MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-			(LPTSTR)&lpMsgBuf,
-			0,
-			NULL
-		);
-		MessageBox(NULL,
-			(LPCTSTR)lpMsgBuf,
-			_T("Windows Desktop Guided Tour"),
-			NULL);
-		LocalFree(lpMsgBuf);
-
 		return 1;
 	}
 
-	hInst = hInstance;
+	// Store instance handle in our global variable
+	// hInst = hInstance;
 
+	// The parameters to CreateWindow explained:
+	// szWindowClass: the name of the application
+	// szTitle: the text that appears in the title bar
+	// WS_OVERLAPPEDWINDOW: the type of window to create
+	// CW_USEDEFAULT, CW_USEDEFAULT: initial position (x, y)
+	// 500, 100: initial size (width, length)
+	// NULL: the parent of this window
+	// NULL: this application does not have a menu bar
+	// hInstance: the first parameter from WinMain
+	// NULL: not used in this application
 	HWND hWnd = CreateWindow(
-		wc_str,
-		title_str,
+		params->cls_name.c_str(),
+		params->title.c_str(),
 		WS_OVERLAPPEDWINDOW,
 		CW_USEDEFAULT, CW_USEDEFAULT,
-		length, height,
+		params->width, params->height,
 		NULL,
 		NULL,
-		hInstance,
+		GetModuleHandle(NULL),
 		NULL
 	);
 
@@ -168,10 +114,58 @@ DWORD WINAPI WindowsStartAsync(LPVOID lpParam)
 		return 1;
 	}
 
+	// The parameters to ShowWindow explained:
+	// hWnd: the value returned from CreateWindow
+	// nCmdShow: the fourth parameter from WinMain
 	ShowWindow(hWnd,
-		nCmdShow);
+		SW_NORMAL);
 	UpdateWindow(hWnd);
 
+	wil::com_ptr<ICoreWebView2Controller> webviewController;
+
+	wil::com_ptr<ICoreWebView2> webview;
+
+	// <-- WebView2 sample code starts here -->
+	// Step 3 - Create a single WebView within the parent window
+	// Locate the browser and set up the environment for WebView
+	CreateCoreWebView2EnvironmentWithOptions(nullptr, nullptr, nullptr,
+		Callback<ICoreWebView2CreateCoreWebView2EnvironmentCompletedHandler>(
+			[hWnd, params, &webviewController, &webview](HRESULT result, ICoreWebView2Environment* env) -> HRESULT {
+
+				// Create a CoreWebView2Controller and get the associated CoreWebView2 whose parent is the main window hWnd
+				env->CreateCoreWebView2Controller(hWnd, Callback<ICoreWebView2CreateCoreWebView2ControllerCompletedHandler>(
+					[hWnd, params, &webviewController,&webview](HRESULT result, ICoreWebView2Controller* controller) -> HRESULT {
+						if (controller != nullptr) {
+							webviewController = controller;
+							webviewController->get_CoreWebView2(&webview);
+						}
+
+						// Add a few settings for the webview
+						// The demo step is redundant since the values are the default settings
+						wil::com_ptr<ICoreWebView2Settings> settings;
+						webview->get_Settings(&settings);
+						settings->put_IsScriptEnabled(TRUE);
+						settings->put_AreDefaultScriptDialogsEnabled(TRUE);
+						settings->put_IsWebMessageEnabled(TRUE);
+
+						// Resize WebView to fit the bounds of the parent window
+						RECT bounds;
+						GetClientRect(hWnd, &bounds);
+						webviewController->put_Bounds(bounds);
+
+						// Schedule an async task to navigate to url
+						webview->Navigate(params->url.c_str());
+
+						return S_OK;
+					}).Get());
+				return S_OK;
+			}).Get());
+
+
+
+	// <-- WebView2 sample code ends here -->
+
+	// Main message loop:
 	MSG msg;
 	while (GetMessage(&msg, NULL, 0, 0))
 	{
@@ -179,33 +173,24 @@ DWORD WINAPI WindowsStartAsync(LPVOID lpParam)
 		DispatchMessage(&msg);
 	}
 
-	global_url_buffer = L"about:blank";
-
-	//UnregisterClass(szWindowClass, hInstance);
+	CoUninitialize();
 
 	return (int)msg.wParam;
 }
 
+// 对外暴露的接口函数
+extern "C" void WindowsStart(const char* window_class, const char* title, int width, int height, const char* url) {
+    auto params = new WindowParams{
+        window_class ? widen(window_class) : L"DefaultWebViewClass",
+        title ? widen(title) : L"WebView2 Window",
+        width > 0 ? width : 800,
+        height > 0 ? height : 600,
+        url ? widen(url) : L"https://www.example.com"
+    };
 
-extern "C" {
-	int CALLBACK WindowsStart(const char* window_class, const char* title, int length, int height, const char* url)
-	{
-		global_window_class = window_class;
-		global_title = title;
-		global_length = length;
-		global_height = height;
-		global_url = url;
-
-
-		HANDLE hThread = CreateThread(NULL, 0, WindowsStartAsync, NULL, 0, NULL); //WaitForSingleObject(hThread, INFINITE);
-		if (hThread != NULL) {
-			CloseHandle(hThread);
-		}
-		return 0;
-	}
+    // 在新线程中创建窗口
+    CreateThread(nullptr, 0, CreateWebView2Window, params, 0, nullptr);
 }
-
-
 int CALLBACK WinMain(
 	_In_ HINSTANCE hInstance,
 	_In_ HINSTANCE hPrevInstance,
@@ -213,49 +198,8 @@ int CALLBACK WinMain(
 	_In_ int       nCmdShow
 )
 {
-	global_hInstance = hInstance; global_hPrevInstance = hPrevInstance; global_lpCmdLine = lpCmdLine; global_nCmdShow = nCmdShow;
 
-	CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
-	CreateCoreWebView2EnvironmentWithOptions(
-		nullptr, nullptr, nullptr,
-		Microsoft::WRL::Callback<ICoreWebView2CreateCoreWebView2EnvironmentCompletedHandler>(
-			[](HRESULT result, ICoreWebView2Environment* env) -> HRESULT {
-				if (SUCCEEDED(result)) {
-					g_webViewEnvironment = env;
-				}
-				return S_OK;
-			}).Get());
-
-	//MessageBox(NULL, L"WinMain called!", L"Debug", MB_OK);
-
-	while (!g_webViewEnvironment);VmStart();
+	VmStart();
 	while (true)Sleep(1000);
-	return 0;
-}
-
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
-	switch (message)
-	{
-	case WM_CREATE:
-		CreateWebView(hWnd);
-		break;
-
-	case WM_SIZE:
-		if (g_webViewControllers[hWnd] != nullptr) {
-			RECT bounds;
-			GetClientRect(hWnd, &bounds);
-			g_webViewControllers[hWnd]->put_Bounds(bounds);
-
-		};
-		break;
-	case WM_DESTROY:
-		PostQuitMessage(0);
-		break;
-	default:
-		return DefWindowProc(hWnd, message, wParam, lParam);
-		break;
-	}
-
 	return 0;
 }
